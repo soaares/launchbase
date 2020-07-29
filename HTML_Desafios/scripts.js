ab0b7a3bda4c47bc759584226f3f8d172a015b0a
Raphael Soares <raphael.soaresit@gmail.com>
2020-07-29 00:55:41 -0300

    Desafios Launchbase

--- HTML_Desafios/scripts.js
+++ HTML_Desafios/scripts.js
@@ -1,12 +1,25 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card_content');
+const modalWhite = document.querySelector('.modal');
 
 for(let card of cards) {
     card.addEventListener('click', function (){
+        const videoId = card.getAttribute('id');
         modalOverlay.classList.add('active');
+        modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoId}`;
+
     })
 }
 
-document.querySelectorAll('.close-modal').addEventListener('click', function(){
+document.querySelector('.close-modal').addEventListener('click', function(){
     modalOverlay.classList.remove('active');
+    modalOverlay.querySelector('iframe').src = '';
+})
+
+document.querySelector('.maximize-modal').addEventListener('click', function(){
+    modalWhite.classList.add('maximize');
+})
+
+document.querySelector('.close-modal').addEventListener('click', function(){
+    modalWhite.classList.remove('maximize');
 })
